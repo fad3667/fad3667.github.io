465c025afd8140ab64edc3227574ee3b3f058e06
fad3667 <64626135+fad3667@users.noreply.github.com>
2020-05-19 14:49:47 +0800

    Add files via upload

--- index.js
+++ index.js
@@ -1,10 +1,19 @@
-function getTime() {
+function showTime() {
   var today = new Date()
   var year = today.getFullYear()
-  var month = today.getMonth()
+  var month = today.getMonth() + 1
   var date = today.getDate()
   var day = today.getDay()
   var hour = today.getHours()
   var minutes = today.getMinutes()
-  return year + '-' + month + '-' + date + ' ' + '星期' + day + ' ' + hour + ':' + minutes
+  var seconds = today.getSeconds()
+  var time = year + '-' + month + '-' + date + ' ' + '星期' + day + ' ' + hour + ':' + minutes + ':' + seconds
+  this.document.getElementById('time-now').innerText = time
+}
+
+window.onload = function() {
+  showTime()
+  this.setInterval(function() {
+    showTime()
+  }, 1000)
 }
